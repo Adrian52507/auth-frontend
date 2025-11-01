9cdc55b889608ed692883b4b4d0ffc0c8c1d1b19
Adrian <adrian52507@gmail.com>
2025-11-01 03:24:05 -0500

    modificando css dashboard 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main style={{ display:"grid", gap:12, padding:24 }}>
-      <h1>Demo Auth</h1>
+      <h1 style={{backgroundColor: "white", color: "black"}}>Demo Auth</h1>
       <a href="/register">Registro</a>
       <a href="/login">Login</a>
       <a href="/dashboard">Dashboard (protegido)</a>
